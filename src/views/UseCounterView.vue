1ed62b074174869c104772f6ab8c41e2e32872d6
yamageji <koyama.shigehito@ceroan.co.jp>
2023-12-28 11:47:35 +0900

    feat: vueUseの導入

---
+++ src/views/UseCounterView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useCounter } from '@vueuse/core'
+
+const { count, inc, dec } = useCounter()
+</script>
+
+<template>
+  <div>
+    <h1 class="text-xl">useCounter</h1>
+    <p class="mt-4">Counter: {{ count }}</p>
+    <div class="flex gap-2 mt-4">
+      <button
+        type="button"
+        @click="inc()"
+        class="rounded grid place-content-center bg-gray-200 min-w-10 min-h-10 p-2 cursor-pointer"
+      >
+        +
+      </button>
+      <button
+        type="button"
+        @click="dec()"
+        class="rounded grid place-content-center bg-gray-200 min-w-10 min-h-10 p-2 cursor-pointer"
+      >
+        -
+      </button>
+    </div>
+  </div>
+</template>
